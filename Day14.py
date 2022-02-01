c41eb1f726b34374a11a964b0f795c543dd133e7
Firdaus85 <52103483+Firdaus85@users.noreply.github.com>
2022-02-01 17:57:02 +0000

    Add files via upload

---
+++ Day14.py
@@ -0,0 +1,28 @@
+# DAY 14 of 100DAYSOFCODE challenge
+# Using learn python 3 the hard way 
+# Today's challenge is strings, bytes and character encodings
+
+import sys
+
+script, input_encoding, error = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors = errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+
+
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, input_encoding, error)
